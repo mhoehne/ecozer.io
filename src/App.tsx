54736016a2037cb835e1530c08880480e532ab54
Martin Hoehne <martin-hoehne@live.de>
2021-10-26 18:55:48 +0200

    added AppBar and counter buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
 import * as React from 'react';
+import { useState } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
+import AppBar from './AppBar';
+import { Button } from '@mui/material';
+
+
+
+
+
+
+export default function App() {
+
+const [count, setCount] = useState(0);
+const incrementCounter = () => {
+  setCount(count+1);
+}
+
+const decreaseCounter = () => {
+  setCount(count-1);
+}
+
+  return (
+    <> {/* fragment */}
+    <AppBar/><Container maxWidth="sm">
+      <Button onClick={incrementCounter} >Count {count}</Button>
+      <Button onClick={decreaseCounter} >-1</Button>
+    <Box sx={{ my: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        React App v5 example with TypeScript
+      </Typography>
+      <ProTip />
+      <Copyright />
+    </Box>
+  </Container>
+  </>
+    
+  );
+}
 
 function Copyright() {
   return (
@@ -14,19 +51,5 @@ function Copyright() {
       </Link>{' '}
       {new Date().getFullYear()}.
     </Typography>
-  );
-}
-
-export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v5 example with TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
   );
 }
