942df69ce9205934e5ec8458c338efcf150112b3
Martin Hoehne <martin-hoehne@live.de>
2021-12-11 16:41:52 +0100

    playing around with grid data

--- src/Components/UserListGrid.tsx
+++ src/Components/UserListGrid.tsx
@@ -1,58 +1,76 @@
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { useEffect, useState } from 'react';
+
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: 'id', 
+    headerName: 'ID', 
+    type: 'number',
+    width: 60,
+    editable: false,
+  },
+  {
+    field: 'emailAddress',
+    headerName: 'E-Mail-Adresse',
+    width: 300,
+    editable: true,
+  },
   {
     field: 'firstName',
-    headerName: 'First name',
+    headerName: 'Vorname',
     width: 150,
     editable: true,
   },
   {
     field: 'lastName',
-    headerName: 'Last name',
+    headerName: 'Nachname',
     width: 150,
     editable: true,
   },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
+    field: 'companyName',
+    headerName: 'Unternehmen',
+    width: 200,
     editable: true,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    field: 'lastLogin',
+    headerName: 'letzter Login',
+    width: 200,
+    editable: false,
   },
 ];
 
+export interface Account {
+  account_id: number;
+  emailAddress: string;
+  firstName: string;
+  lastName: string;
+  companyName: string;
+  lastLogin: string;
+}
+
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, emailAddress: 'atryrtytry@uahsduhs.de', lastName: 'Snow', firstName: 'Jon', companyName: 'JS GmbH', lastLogin: '00-00-2021' },
+  { id: 2, emailAddress: 'uioyuddf@uahsduhs.de', lastName: 'Lannister', firstName: 'Cersei', companyName: 'CL GmbH', lastLogin: '00-00-2021'},
+  { id: 3, emailAddress: 'asdfgdfgfdg@uahsduhs.de', lastName: 'Lannister', firstName: 'Jaime', companyName: 'JL GmbH', lastLogin: '00-00-2021'},
+  { id: 4, emailAddress: 'asdgfd564@uahsduhs.de', lastName: 'Stark', firstName: 'Arya', companyName: 'AS GmbH', lastLogin: '00-00-2021'},
+  { id: 5, emailAddress: 'nvbdfgdg53@uahsduhs.de', lastName: 'Targaryen', firstName: 'Daenerys', companyName: 'TD GmbH', lastLogin: '00-00-2021'},
+  { id: 6, emailAddress: 'ytuert5654@uahsduhs.de', lastName: 'Melisandre', firstName: null, companyName: 'M GmbH', lastLogin: '00-00-2021'},
+  { id: 7, emailAddress: 'sdgdf564645@uahsduhs.de', lastName: 'Clifford', firstName: 'Ferrara', companyName: 'FC GmbH', lastLogin: '00-00-2021'},
+  { id: 8, emailAddress: 'ahjghkrt452@uahsduhs.de', lastName: 'Frances', firstName: 'Rossini', companyName: 'RF GmbH', lastLogin: '00-00-2021'},
+  { id: 9, emailAddress: 'iuytyutyr@uahsduhs.de', lastName: 'Roxie', firstName: 'Harvey', companyName: 'HR GmbH', lastLogin: '00-00-2021' },
+  { id: 10, emailAddress: 'asuidhud@uahsduhs.de', firstName: 'Pablo', lastName: 'Testobar', companyName: 'Pablo import export GmbH', lastLogin: '00-00-2021'},
 ];
 
 export default function DataGridDemo() {
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: '100vh', width: '100%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
+        pageSize={100}
+        rowsPerPageOptions={[100]}
         disableSelectionOnClick
       />
     </div>
