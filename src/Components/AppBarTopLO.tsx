24732e94e781f37e6d32c0c959faccf301f28d3a
Martin Hoehne <martin-hoehne@live.de>
2021-12-13 13:07:50 +0100

    created an added HeroBanner to the HomePage / created AppBarTopLO when logged out

---
+++ src/Components/AppBarTopLO.tsx
@@ -0,0 +1,86 @@
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchBar from './SearchBar';
+import Logo from '../images/Q04_HTW_Berlin_Logo_quer_pos_FARBIG_RGB.jpg';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+
+const drawerWidth = 240;
+
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+
+
+type AppBarTopProps = {open: boolean, setOpen: Function}
+
+export default function AppBarTopLO(props: AppBarTopProps) {
+
+  return (
+    <Box sx={{ display: 'flex' }} >
+      <CssBaseline />
+      <AppBar position="fixed" open={props.open} sx= {{backgroundColor: "#fff"}}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={ () => props.setOpen(true) }
+            edge="start"
+            sx={{
+              fontSize: 'large',
+              marginRight: '36px',
+              ...(props.open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+          <CardMedia
+            component="img"
+            sx={{maxHeight: '40px'}}
+            alt="green iguana"
+            image={Logo}
+          />
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <SearchBar/>
+          
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <Button variant="contained" size="small">
+          Signup
+          </Button>
+          <Button variant="outlined" size="small">
+          SignIn
+          </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,16 +1,11 @@
 import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import { Button, Paper, Stack } from '@mui/material';
 import ImgMediaCard from '../Components/ImgMediaCard';
 import Grid from '@mui/material/Grid';
 import Navigation from '../Components/Navigation';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import earth_image from '../images/earth_image.png';
-import EmailTextField from '../Components/EmailTextField';
 import Footer from '../Components/Footer';
+import HeroBanner from '../Components/HeroBanner';
 
 
 
@@ -32,11 +27,8 @@ Item.defaultProps = {
   return (
     < > {/* fragment */}
     <Navigation />
-    
-    
+    <HeroBanner/>
     <Container>
-    
-
       <Grid container spacing={0}>
       <Grid item xs={4}>
         <ImgMediaCard/>
