4dacbfedb73a818c507f2fb82a5af79e728fb089
Martin Hoehne <martin-hoehne@live.de>
2021-12-27 22:10:07 +0100

    adjusted chip size to small

--- src/Components/ImgMediaCard.tsx
+++ src/Components/ImgMediaCard.tsx
@@ -50,11 +50,11 @@ export default function ImgMediaCard() {
           <Typography>Ausprägungen</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Chip color="primary" icon={<FaceIcon />} label="Stand-Alone" deleteIcon={<DoneIcon />} sx={{mr:1, mb:1}} />
-          <Chip color="primary" icon={<FaceIcon />} label="Emission" deleteIcon={<DoneIcon />} sx={{mr:1, mb:1}} />
-          <Chip color="primary" icon={<FaceIcon />} label="Abfall" deleteIcon={<DoneIcon />} sx={{mr:1, mb:1}} />
-          <Chip color="primary" icon={<FaceIcon />} label="Management" deleteIcon={<DoneIcon />} sx={{mr:1, mb:1}} />
-          <Chip color="primary" icon={<FaceIcon />} label="Zertifizierung" deleteIcon={<DoneIcon />} sx={{mr:1, mb:1}} />
+          <Chip color="primary" size="small" icon={<FaceIcon />} label="Stand-Alone" deleteIcon={<DoneIcon />} sx={{mr:1, mb:1}} />
+          <Chip color="primary" size="small" icon={<FaceIcon />} label="Emission" deleteIcon={<DoneIcon />} sx={{mr:1, mb:1}} />
+          <Chip color="primary" size="small" icon={<FaceIcon />} label="Abfall" deleteIcon={<DoneIcon />} sx={{mr:1, mb:1}} />
+          <Chip color="primary" size="small" icon={<FaceIcon />} label="Management" deleteIcon={<DoneIcon />} sx={{mr:1, mb:1}} />
+          <Chip color="primary" size="small" icon={<FaceIcon />} label="Zertifizierung" deleteIcon={<DoneIcon />} sx={{mr:1, mb:1}} />
         </AccordionDetails>
       </Accordion>
       
