36613af4e0c2efc7bae68101270ad84d7d388c08
Martin Hoehne <martin-hoehne@live.de>
2022-01-01 18:13:07 +0100

    added questions for research

--- src/Pages/1newPageTemplate.tsx
+++ src/Pages/1newPageTemplate.tsx
@@ -18,9 +18,9 @@ export default function PageName() {
 {/* /my-products ImgMediaCards should have the same size everywhere => create own grid component with left & main panel */}
 
 {/* /user-list how do I style the list in zebra style? */}
-{/* */}
-{/* */}
-{/* */}
+{/* /user-list how can I change the background color from just admins? */}
+{/* How can I make pagination work? */}
+{/* How should the reset password admin function should look and work? */}
 {/* */}
 {/* */}
 {/* */}
