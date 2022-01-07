19cc1012c4c1fb25ee633127611d421d79f38c4a
Martin Hoehne <martin-hoehne@live.de>
2022-01-07 20:22:00 +0100

    adjusted product detail page

--- src/Pages/ProductDetail.tsx
+++ src/Pages/ProductDetail.tsx
@@ -2,9 +2,10 @@ import Navigation from '../Components/Navigation';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import { Divider, Paper } from '@mui/material';
-import MorphologicalBox from '../Components/MorphologicalBox';
+import Box from '@mui/material/Box';
 import ProductDetailViewCard from '../Components/ProductDetailViewCard';
 import MorphologicalBoxVertical from '../Components/MorphologicalBoxVertical';
+import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 
 
 export default function ProductDetail() {
@@ -13,10 +14,13 @@ export default function ProductDetail() {
   return (
 
       <>
-          <ProductDetailViewCard/>
-          <MorphologicalBox/>
-          <MorphologicalBoxVertical/>
-          <Paper/>
+          <Box sx={{mt: 10}}>
+          <Button variant="text" color="info" startIcon={<ArrowBackIosNewOutlinedIcon/>} href="/approval">
+            Zurück
+          </Button>
+        </Box>
+        <ProductDetailViewCard/>
+        <MorphologicalBoxVertical/>
       </>
   );
 }
