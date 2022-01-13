5f09de4a032543ebb00f32ba5c39413ca2afd054
Martin Hoehne <martin-hoehne@live.de>
2022-01-13 19:50:10 +0100

    expanded the API with productType

--- src/API.ts
+++ src/API.ts
@@ -16,5 +16,31 @@ export type AccountResultType = {
 export function GetAccounts(): Promise<AxiosResponse<AccountResultType>>
 
 {
-return axios.get<AccountResultType>('http://localhost:8000/accounts');
+  return axios.get<AccountResultType>('http://localhost:8000/accounts');
+  }
+
+export type ProductType = {
+  product_id: number;
+  account_id: number;
+  productName: string;
+  productImage: string;
+  productLink: string;
+  productCompany: string;
+  productDescription: string;
+  zielgruppe: string;
+  anwendungsbereich: string;
+  gradDerIntegrierung: string;
+  objektAspekt: string;
+  systemgrenzen: string;
+  betrachtungskonzept: string;
 }
+
+export type ProductResultType = {
+  products: ProductType[],
+}
+
+export function GetProducts(): Promise<AxiosResponse<ProductResultType>>
+
+{
+  return axios.get<ProductResultType>('http://localhost:8000/products');
+  }
