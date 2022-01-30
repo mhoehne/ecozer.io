39a4b1310c82a27c48142d197a7efe638f4c6027
Martin Hoehne <martin-hoehne@live.de>
2022-01-30 17:46:49 +0100

    cleaned up a bit

--- src/API.ts
+++ src/API.ts
@@ -15,16 +15,37 @@ export type AccountResultType = {
 
 export type AccountCreatedResultType = AccountsType | string
 
+/*CRUD*SECTION>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+/*CREATE*************************************************************************************************/
+
 export function CreateAccount(accounts: AccountsType): Promise<AxiosResponse<AccountCreatedResultType>> {
 
   return axios.post<AccountCreatedResultType>('http://localhost:8000/accounts', accounts,);
 }
+
+/*READ***************************************************************************************************/
 
 export function GetAccounts(): Promise<AxiosResponse<AccountResultType>>
 
 {
   return axios.get<AccountResultType>('http://localhost:8000/accounts');
   }
+
+/*UPDATE*************************************************************************************************/
+
+export function PutAccounts(): Promise<AxiosResponse<AccountResultType>>
+{
+  return axios.put<AccountResultType>('http://localhost:8000/accounts');
+}
+
+/*DELETE*************************************************************************************************/
+
+export function DeleteAccounts(): Promise<AxiosResponse<AccountResultType>>
+{
+  return axios.put<AccountResultType>('http://localhost:8000/accounts');
+}
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<CRUD*SECTION*/
 
 export type ProductType = {
   product_id: number;
