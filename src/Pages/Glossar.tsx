cf02ba79706d94dca00c4b13358f4bcd149141fc
Martin Hoehne <martin-hoehne@live.de>
2022-02-05 16:30:35 +0100

    test on Glossar page

--- src/Pages/Glossar.tsx
+++ src/Pages/Glossar.tsx
@@ -2,6 +2,7 @@ import react from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
 
 
 export default function Glossar() {
@@ -10,6 +11,8 @@ export default function Glossar() {
   return (
 
       <>
+        <Container sx={{mt: 20}}/>
+
       </>
   );
 }
