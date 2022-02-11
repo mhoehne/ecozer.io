789cb558ba5f1d32bdf397363c8397d7fe45caff
Martin Hoehne <martin-hoehne@live.de>
2022-02-11 20:28:52 +0100

    modified Glossar page

--- src/Pages/Glossar.tsx
+++ src/Pages/Glossar.tsx
@@ -25,7 +25,31 @@ export default function Glossar() {
               h6. Heading
             </Typography>
             <Typography variant="body2" gutterBottom>
+              body2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis iusto quos iure omnis ex ullam? Et eius voluptas quia vel totam dicta, fuga blanditiis, eveniet, dolores assumenda repudiandae placeat! Culpa.
+            </Typography>
+        </Box>
+
+        <Box 
+        mx={{ xs: 4, sm: 4}}
+        my={{ xs: 2, sm: 2}}
+        px={{ xs: 2, sm: 2}}
+        py={{ xs: 2, sm: 2}}
+        bgcolor="background.paper"
+        color="text.primary">
+            <Typography variant="h6" gutterBottom component="div">
+              h6. Heading
+            </Typography>
+            <Typography variant="body2" gutterBottom>
               body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+              neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+              quasi quidem quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+              neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+              quasi quidem quibusdam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+              neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+              quasi quidem quibusdam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
               blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
               neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
               quasi quidem quibusdam.
@@ -82,22 +106,22 @@ export default function Glossar() {
               body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
               blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
               neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-              quasi quidem quibusdam.
-            </Typography>
-        </Box>
-
-        <Box 
-        mx={{ xs: 4, sm: 4}}
-        my={{ xs: 2, sm: 2}}
-        px={{ xs: 2, sm: 2}}
-        py={{ xs: 2, sm: 2}}
-        bgcolor="background.paper"
-        color="text.primary">
-            <Typography variant="h6" gutterBottom component="div">
-              h6. Heading
-            </Typography>
-            <Typography variant="body2" gutterBottom>
-              body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              quasi quidem quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+              neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+              quasi quidem quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+              neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+              quasi quidem quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+              neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+              quasi quidem quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+              neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+              quasi quidem quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+              blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+              neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+              quasi quidem quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
               blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
               neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
               quasi quidem quibusdam.
@@ -118,7 +142,11 @@ export default function Glossar() {
               body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
               blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
               neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-              quasi quidem quibusdam.
+              quasi quidem quibusdam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex soluta, nemo a facere at accusamus? Nulla possimus dolor quo, cupiditate quos officiis sunt inventore magni, iusto commodi pariatur qui? Id!
+              Odio vitae deserunt provident quia quasi? Nobis quo id sit architecto nam? Repellat eius at error nisi reiciendis eum, modi voluptatem esse est quasi consectetur mollitia rerum, sunt et nobis?
+              Doloribus omnis animi pariatur facere saepe quis tempore eveniet laudantium harum iure magnam reiciendis accusamus, minima, ullam, corporis nesciunt tempora numquam velit explicabo incidunt repudiandae dolores? Deserunt error libero expedita!
+              Aut labore, vel accusantium, veritatis fugiat accusamus illum dolore minima fuga explicabo numquam rerum sapiente! Doloremque ipsa omnis officia libero neque minus vero, accusamus dolorum est nam deserunt, ut perspiciatis.
+              Accusantium praesentium omnis beatae sunt doloremque inventore architecto fugiat aliquam? Optio alias voluptas, modi voluptate amet rerum molestias quas. Aut excepturi veniam libero architecto voluptates ut animi velit enim quod.
             </Typography>
         </Box>
 
