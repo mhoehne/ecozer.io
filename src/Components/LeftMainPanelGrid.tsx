1f693eb8a55fc134109db04d9af1ca2ff58ba1d6
Martin Hoehne <martin-hoehne@live.de>
2022-02-12 16:31:16 +0100

    modified leftmain panel grid

--- src/Components/LeftMainPanelGrid.tsx
+++ src/Components/LeftMainPanelGrid.tsx
@@ -9,15 +9,78 @@ export default function LeftMainPanelGrid() {
   return (
 
     <>
-      <Box 
-          px={{ xs: 2, sm: 2}}
-          py={{ xs: 2, sm: 2}}
-          mx={{ xs: 0, sm: 0}}
-          my={{ xs: 2, sm: 2}}
-          bgcolor="background.paper"
-          color="text.primary">
+      <Grid
+        container
+        rowSpacing={0} 
+        columnSpacing={{ xs: 0, sm: 0, md: 0 }}
+        >
 
-      </Box>
+          <Grid
+          item xs={4} sm={4}
+          height="50rem"
+          direction="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          >
+            <Box  
+              px={{ xs: 2, sm: 2}}
+              py={{ xs: 0, sm: 0}}
+              mx={{ xs: 4, sm: 4}}
+              my={{ xs: 4, sm: 4}}
+              bgcolor="background.paper"
+              color="text.primary">
+                0
+            </Box>
+          </Grid>
+
+
+          <Grid
+            item xs={8} sm={8}
+            height="50rem"
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            > 
+              <Box  
+              px={{ xs: 2, sm: 2}}
+              py={{ xs: 0, sm: 0}}
+              mx={{ xs: 4, sm: 4}}
+              my={{ xs: 4, sm: 4}}
+              bgcolor="background.paper"
+              color="text.primary">
+                <Grid item xs={12} sm={6}>
+                  1
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  2
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  3
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  4
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  5
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  6
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  7
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  8
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  9
+                </Grid>
+                </Box>
+              </Grid>
+
+
+
+        </Grid>
 
     </>
   );
--- src/Pages/00testpage.tsx
+++ src/Pages/00testpage.tsx
@@ -2,6 +2,8 @@ import react from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import LeftMainPanelGrid from '../Components/LeftMainPanelGrid';
+
 
 {/* */}
 
@@ -11,6 +13,7 @@ export default function TestPage() {
   return (
 
       <>
+      <LeftMainPanelGrid/>
       </>
   );
 }
