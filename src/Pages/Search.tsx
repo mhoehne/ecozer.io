6a142d7bfdb6e6e98ea29f6fcd1c30464a8984bd
Martin Hoehne <martin-hoehne@live.de>
2022-03-03 19:26:32 +0100

    added products to the search page

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -3,10 +3,27 @@ import ImageBanner from '../Components/ImageBanner';
 import Box from '@mui/material/Box';
 import SearchBarBoxed from '../Components/SearchBarBoxed';
 import AccordionFilter from '../Components/AccordionFilter';
+import ImgMediaCard from '../Components/ImgMediaCard';
+import AddProductCard from '../Components/AddProductCard';
+import { 
+  useEffect, 
+  useState,
+} from 'react';
+import { 
+  GetProducts, 
+  ProductType
+} from '../API';
 
 
 export default function Search() {
 
+  const [products, setProducts] = useState<ProductType[]>([])
+
+  useEffect(() => {
+    GetProducts()
+    .then((result) => {setProducts(result.data.products)})
+    .catch();
+  },[]);
 
   return (
 
@@ -40,8 +57,15 @@ export default function Search() {
           <Grid 
             container
             item xs={8} sm={8}
+            spacing={0}
             > 
-              
+              {
+            products.map((product) => {
+              return <Grid item key={product._id} xs={12} sm={6}>
+                <ImgMediaCard Product={product}/> 
+              </Grid>
+            })
+          }
               </Grid>
 
 
