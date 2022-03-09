f3442d550941edeab44127d115efb8a6a6171516
Martin Hoehne <martin-hoehne@live.de>
2022-03-09 13:53:59 +0100

    modified user dashboard layout

---
+++ src/Components/Dashboard/UserProducts.tsx
@@ -0,0 +1,109 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText
+} from '@mui/material';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+const products = [
+  {
+    name: 'Dropbox',
+    imageUrl: '/static/images/products/product_1.png',
+  },
+  {
+    name: 'Medium Corporation',
+    imageUrl: '/static/images/products/product_2.png',
+  },
+  {
+    name: 'Slack',
+    imageUrl: '/static/images/products/product_3.png',
+  },
+  {
+    name: 'Lyft',
+    imageUrl: '/static/images/products/product_4.png',
+  },
+  {
+    name: 'GitHub',
+    imageUrl: '/static/images/products/product_5.png',
+  }
+];
+
+
+export default function UserProducts() {
+
+
+  return (
+
+    <>
+      <Box 
+          px={{ xs: 2, sm: 2}}
+          py={{ xs: 2, sm: 2}}
+          mx={{ xs: 0, sm: 0}}
+          my={{ xs: 2, sm: 2}}
+          bgcolor="background.paper"
+          color="text.primary">
+
+      </Box>
+      <Card >
+    <CardHeader
+      subtitle={`${products.length} in total`}
+      title="Latest Products"
+    />
+    <Divider />
+    <List>
+      {products.map((product, i) => (
+        <ListItem
+          divider={i < products.length - 1}
+        >
+          <ListItemAvatar>
+            <img
+              alt={product.name}
+              src={product.imageUrl}
+              style={{
+                height: 48,
+                width: 48
+              }}
+            />
+          </ListItemAvatar>
+          <ListItemText
+            primary={product.name}
+          />
+          <IconButton
+            edge="end"
+            size="small"
+          >
+            <MoreVertIcon />
+          </IconButton>
+        </ListItem>
+      ))}
+    </List>
+    <Divider />
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        p: 2
+      }}
+    >
+      <Button
+        color="primary"
+        endIcon={<ArrowRightIcon />}
+        size="small"
+        variant="text"
+      >
+        View all
+      </Button>
+    </Box>
+  </Card>
+
+    </>
+  );
+}
--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,8 +1,15 @@
 import react from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import 
+{ 
+  Box, 
+  Container, 
+  Grid 
+} from '@mui/material';
 import Typography from '@mui/material/Typography';
 import LeftMainPanelGrid from '../Components/LeftMainPanelGrid';
+import TotalVisits from '../Components/Dashboard/TotalVisits';
+import LatestVisits from '../Components/Dashboard/LatestVisits';
+import UserProducts from '../Components/Dashboard/UserProducts';
 
 
 export default function Dashboard() {
@@ -10,8 +17,96 @@ export default function Dashboard() {
 
   return (
 
-      <>
-        <LeftMainPanelGrid/>
-      </>
+    <>
+      <title>
+        Dashboard
+      </title>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <TotalVisits/>
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalVisits/>
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalVisits/>
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalVisits/>
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            <LatestVisits/>
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+            
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+            <UserProducts/>
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
   );
 }
