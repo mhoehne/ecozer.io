113203ad6cae7f88f68a6ae11408640a7aa2c797
Martin Hoehne <martin-hoehne@live.de>
2022-06-29 18:09:33 +0200

    correction

--- src/Components/ImageFileDrop/FileDrop.tsx
+++ src/Components/ImageFileDrop/FileDrop.tsx
@@ -1,20 +1,29 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 
-export default function MyDropzone() {
+export default function ImageFileUpload() {
   const onDrop = useCallback((acceptedFiles) => {
-    // Do something with the files
+    console.log(acceptedFiles);
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    multiple: false,
+    maxSize: 2097152,
+    accept: {
+      'image/*': ['.png', '.gif', '.jpeg', '.jpg'],
+    },
+  });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
-    </div>
+    <section className="container">
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+    </section>
   );
 }
