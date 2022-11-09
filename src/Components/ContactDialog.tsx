84df0d5691ec5442225c0faed0bba71db0810077
Martin Hoehne <martin-hoehne@live.de>
2022-11-09 19:36:25 +0100

    adding formik to the contact dialog

--- src/Components/ContactDialog.tsx
+++ src/Components/ContactDialog.tsx
@@ -1,3 +1,4 @@
+import { Field, Form, Formik } from 'formik';
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -84,102 +85,117 @@ export default function ContactDialog(props: ContactDialogProps) {
     <>
       <Dialog open={props.open}>
         <DialogTitle>Kontakt</DialogTitle>
-        <Box
-          component="form"
-          noValidate
-          justifyContent="center"
-          alignItems="center"
-          // onSubmit={handleSubmit}
+        <Formik
+          initialValues={{
+            name: '',
+            email: '',
+            issue: '',
+            browser: '',
+            pageName: '',
+            feedbackField: '',
+          }}
+          onSubmit={async (values) => {
+            await new Promise((r) => setTimeout(r, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
         >
-          <DialogContent>
-            {/* <DialogContentText></DialogContentText> */}
+          <Box
+            component="form"
+            noValidate
+            justifyContent="center"
+            alignItems="center"
+            // onSubmit={handleSubmit}
+          >
+            <DialogContent>
+              {/* <DialogContentText></DialogContentText> */}
 
-            <TextField
-              autoFocus
-              required
-              margin="dense"
-              label="Name"
-              type="name"
-              fullWidth
-              variant="outlined"
-              sx={{ mb: 2 }}
-            />
-            <TextField
-              autoFocus
-              required
-              margin="dense"
-              label="Email Address"
-              type="email"
-              fullWidth
-              variant="outlined"
-              sx={{ mb: 2 }}
-            />
-            <TextField
-              id="outlined-select-type"
-              required
-              select
-              label="Anliegen"
-              type="text"
-              fullWidth
-              value={types}
-              sx={{ mb: 1 }}
-              onChange={handleChange}
-            >
-              {types.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
-            <TextField
-              autoFocus
-              margin="dense"
-              label="Browser"
-              type="text"
-              fullWidth
-              variant="outlined"
-              sx={{ mb: 1 }}
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              label="Seitenname"
-              type="text"
-              fullWidth
-              variant="outlined"
-              sx={{ mb: 1 }}
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              label="Beschreibung"
-              type="text"
-              fullWidth
-              variant="outlined"
-              multiline
-              rows={8}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button
-              onClick={() => {
-                props.setOpen(false);
-              }}
-            >
-              zurück
-            </Button>
-            <Button
-              type="submit"
-              variant="contained"
-              onClick={() => {
-                props.setOpen(false);
-              }}
-              sx={{ color: 'background.paper' }}
-            >
-              Senden
-            </Button>
-          </DialogActions>
-        </Box>
+              <TextField
+                autoFocus
+                required
+                margin="dense"
+                label="Name"
+                type="name"
+                fullWidth
+                variant="outlined"
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                autoFocus
+                required
+                margin="dense"
+                label="Email Address"
+                type="email"
+                fullWidth
+                variant="outlined"
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                id="outlined-select-type"
+                required
+                select
+                label="Anliegen"
+                type="text"
+                fullWidth
+                value={types}
+                sx={{ mb: 1 }}
+                onChange={handleChange}
+              >
+                {types.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Browser"
+                type="text"
+                fullWidth
+                variant="outlined"
+                sx={{ mb: 1 }}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Seitenname"
+                type="text"
+                fullWidth
+                variant="outlined"
+                sx={{ mb: 1 }}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Beschreibung"
+                type="text"
+                fullWidth
+                variant="outlined"
+                multiline
+                rows={8}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={() => {
+                  props.setOpen(false);
+                }}
+              >
+                zurück
+              </Button>
+              <Button
+                type="submit"
+                variant="contained"
+                onClick={() => {
+                  props.setOpen(false);
+                }}
+                sx={{ color: 'background.paper' }}
+              >
+                Senden
+              </Button>
+            </DialogActions>
+          </Box>
+        </Formik>
       </Dialog>
     </>
   );
