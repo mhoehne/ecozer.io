86d8ab3f176d1f0d8b0b2ead8b6dd25a3232e649
Martin Hoehne <martin-hoehne@live.de>
2023-01-24 22:03:10 +0100

    trying to show notifications on the data grid, few errors to solve

--- src/Pages/NotificationLog.tsx
+++ src/Pages/NotificationLog.tsx
@@ -9,7 +9,10 @@ import {
     GridToolbarDensitySelector
 } from '@mui/x-data-grid';
 
-import { AccountType, DeleteProduct, GetProducts, ProductType, PutProducts } from '../API';
+import {
+    AccountType, DeleteProduct, GetNotifications, GetProducts, NotificationsType, NotificationType,
+    ProductType, PutProducts
+} from '../API';
 import AssignProduct from '../Components/AssignProduct';
 import Account from '../Pages/Account';
 
@@ -21,91 +24,28 @@ function CustomToolbar() {
   );
 }
 
-const onRowEdit = (products: ProductType[], state: GridEditRowsModel) => {
-  for (const _id in state) {
-    const productNewFields: { [key: string]: string | number } = {
-      _id: parseInt(_id),
-    };
-
-    // Get all new values from `state` into `productNewFields`
-    for (const field in state[_id]) {
-      const newValue = state[_id][field].value?.toString();
-      if (newValue === undefined) {
-        continue;
-      }
-
-      productNewFields[field] = newValue;
-    }
-
-    // Find an product inside `products` state variable
-    let product = products.find((acc) => acc._id === parseInt(_id));
-
-    // Did not find an product: should never happen. Send error to console and skip this email to avoid a crash.
-    if (product === undefined) {
-      console.error(
-        `Could not find an product with ID "${_id}". This is most likely a bug.`
-      );
-      continue;
-    }
-
-    // Paste all fields inside `productNewFields` into `product`
-    product = Object.assign(product, productNewFields);
-
-    // Update `product`
-    PutProducts(product)
-      .then(() => {
-        // TODO: products updated successfuly
-        // trigger success snackbar alert
-      })
-      .catch(() => {
-        // could not update products
-        // trigger error snackbar alert
-        console.error(`Could not update the product with ID "${_id}"`);
-      });
-  }
-};
-
 interface NotificationLogProps {
   account: AccountType | null;
 }
 
 export default function NotificationLog(props: NotificationLogProps) {
   const [pageSize, setPageSize] = useState<number>(20);
-  const [open, setOpen] = useState(false);
-  const [products, setProducts] = useState<ProductType[]>([]);
-  const [product, setProduct] = useState<ProductType>();
+  const [notifications, setNotifications] = useState<NotificationsType[]>([]);
+  const [notification, setNotification] = useState<NotificationType>();
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
   const handleClickOpen = (id: number) => () => {
-    let product = products.find((p) => p._id === id);
-    if (product === undefined) {
+    let notification = notifications.find((p) => p.notifications === id);
+    if (notification === undefined) {
       console.error(
         `Could not find a product with ID "${id}". This is most likely a bug.`
       );
       return;
     }
-    setOpen(true);
-    setProduct(product);
-  };
-  const handleClose = () => {
-    setOpen(false);
   };
 
   const columns = [
-    {
-      field: 'actions',
-      type: 'actions',
-      width: 40,
-      getActions: (params: { id: string }) => [
-        <GridActionsCellItem
-          icon={<AssignmentIndOutlinedIcon />}
-          label="Zuweisen"
-          onClick={handleClickOpen(parseInt(params.id))}
-          showInMenu
-        />,
-      ],
-    },
     {
       field: '_id',
       headerName: 'ID',
@@ -114,42 +54,17 @@ export default function NotificationLog(props: NotificationLogProps) {
       editable: false,
     },
     {
-      field: 'state',
+      field: 'messageType',
       headerName: 'Status',
       width: 100,
       editable: false,
     },
-
-    {
-      field: 'productName',
-      headerName: 'Produktname',
-      width: 250,
-      editable: true,
-    },
-    {
-      field: 'productLink',
-      headerName: 'Website-Link',
-      width: 200,
-      editable: true,
-    },
-    {
-      field: 'productCompany',
-      headerName: 'Firmenname',
-      width: 200,
-      editable: true,
-    },
-    {
-      field: 'productDescription',
-      headerName: 'Beschreibung',
-      width: 500,
-      editable: true,
-    },
   ];
 
   useEffect(() => {
-    GetProducts(null, null, [], [], [], [], [], [], null)
+    GetNotifications()
       .then((result) => {
-        setProducts(result.data.products);
+        setNotifications(result.data.notifications);
       })
       .catch();
   }, []);
@@ -180,10 +95,9 @@ export default function NotificationLog(props: NotificationLogProps) {
         <div style={{ height: '80vh', width: '100%' }}>
           <DataGrid
             localeText={deDE.components.MuiDataGrid.defaultProps.localeText}
-            rows={products}
+            rows={notifications}
             columns={columns}
-            getRowId={(product) => product._id}
-            onEditRowsModelChange={(state) => onRowEdit(products, state)}
+            getRowId={(notification) => notification._id}
             disableSelectionOnClick
             density="standard"
             components={{
@@ -195,14 +109,6 @@ export default function NotificationLog(props: NotificationLogProps) {
           />
         </div>
       </Box>
-      {product ? (
-        <AssignProduct
-          open={open}
-          handleClose={handleClose}
-          Account={props.account}
-          Product={product}
-        />
-      ) : null}
     </>
   );
 }
